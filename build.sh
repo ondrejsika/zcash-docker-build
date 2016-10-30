2bc0dbc4b480b2e93e0ab7c10614862f9083688b
Ondrej Sika <ondrej@ondrejsika.com>
2016-10-30 22:23:22 +0100

    fix variable name

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 set -e
 
 IMAGETAG=zcash-build
-FIRMWARETAG=${1:-master}
+REVISION=${1:-master}
 
 docker build -t $IMAGETAG .
 docker run -t -v $(pwd)/output:/output $IMAGETAG /bin/sh -c "\
     git clone https://github.com/zcash/zcash.git && \
     cd zcash/ && \
-    git checkout $FIRMWARETAG && \
+    git checkout $REVISION && \
     ./zcutil/build.sh -j$(nproc) && \
     cp ./src/zcashd /output && \
     cp ./src/zcash-cli /output
